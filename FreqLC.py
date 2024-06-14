4cc03a3ad387baa9856a9b02701dcfa201dd934d
INVOKERer <mxt_invoker1997@163.com>
2024-06-14 12:09:08 +0800

    Add files via upload

---
+++ FreqLC.py
@@ -0,0 +1,206 @@
+from dct_util import *
+from einops import rearrange
+import numbers
+import math
+import torch.nn.functional as F
+def d4_to_3d(x):
+    return rearrange(x, 'b c h w -> b (h w) c')
+
+def d3_to_4d(x,h,w):
+    return rearrange(x, 'b (h w) c -> b c h w',h=h,w=w)
+def d5_to_3d(x):
+    # x = x.permute(0, 2, 1, 3, 4)
+    return rearrange(x, 'b c s h w -> b (s h w) c')
+
+
+def d3_to_5d(x, s, h, w):
+    x = rearrange(x, 'b (s h w) c -> b c s h w', s=s, h=h, w=w)
+    # x = x.permute(0, 2, 1, 3, 4)
+    return x
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, bias, mu_sigma=False):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+        self.mu_sigma = mu_sigma
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.norm_bias = bias
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+
+        mu = x.mean(-1, keepdim=True)
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        if self.norm_bias:
+            x = (x - mu) / torch.sqrt(sigma+1e-5) * self.weight + self.bias
+        else:
+            x = (x - mu) / torch.sqrt(sigma+1e-5) * self.weight
+        if self.mu_sigma:
+            return x, mu, sigma
+        else:
+            return x
+class LayerNorm(nn.Module):
+    def __init__(self, dim, bias=True, mu_sigma=False, out_dir=None):
+        super(LayerNorm, self).__init__()
+        self.mu_sigma = mu_sigma
+        self.body = WithBias_LayerNorm(dim, bias, mu_sigma)
+        self.out_dir = out_dir
+        # if self.out_dir:
+        #     self.dct = DCT2x()
+
+    def forward(self, x):
+        # if self.out_dir:
+        #     save_feature(self.out_dir+'/beforeLN', x, min_max='log')
+        h, w = x.shape[-2:]
+        x = d4_to_3d(x)
+
+        if self.mu_sigma:
+            x, mu, sigma = self.body(x)
+            return d3_to_4d(x, h, w), d3_to_4d(mu, h, w), d3_to_4d(sigma, h, w)
+        else:
+            x = self.body(x)
+            # if self.out_dir:
+            #     x = d3_to_4d(x, h, w)
+            #     save_feature(self.out_dir + '/afterLN', x, min_max='log')
+            #     return x
+            # else:
+            return d3_to_4d(x, h, w)
+
+def check_image_size(x, padder_size, mode='reflect'):
+    _, _, h, w = x.size()
+    if isinstance(padder_size, int):
+        padder_size_h = padder_size
+        padder_size_w = padder_size
+    else:
+        padder_size_h, padder_size_w = padder_size
+    mod_pad_h = (padder_size_h - h % padder_size_h) % padder_size_h
+    mod_pad_w = (padder_size_w - w % padder_size_w) % padder_size_w
+    x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h), mode=mode)
+    return x
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads, bias, window_size=8, padding_mode='zeros'):
+        super().__init__()
+        self.num_heads = num_heads
+        self.window_size = window_size
+
+        self.mlp = nn.Sequential(
+            nn.Linear(window_size**2, window_size**2, bias=True),
+            nn.GELU(),
+        )
+
+        self.qkv = nn.Conv2d(dim, dim * 3, kernel_size=1, bias=bias)
+
+        self.qkv_dwconv = nn.Conv2d(dim * 3, dim * 3, kernel_size=3,
+                                stride=1, padding=1, groups=dim * 3, bias=bias, padding_mode=padding_mode)
+
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1) / math.sqrt(dim))
+
+
+        self.project_out = nn.Conv2d(dim, dim, kernel_size=1, bias=True)
+
+
+    def get_attn(self, qkv):
+        H, W = qkv.shape[-2:]
+        qkv = check_image_size(qkv, self.window_size)
+        Hx, Wx = qkv.shape[-2:]
+        qkv = rearrange(qkv, 'b (z head c) (h1 h) (w1 w) -> z (b h1 w1) head c (h w)', z=3, head=self.num_heads,
+                        h=self.window_size, w=self.window_size)
+
+        q, k, v = qkv[0], qkv[1], qkv[2]
+        attn = (q @ k.transpose(-2, -1)) * self.temperature
+
+        attn = attn.softmax(dim=-1)
+        out = (attn @ v)
+
+        out = out * self.mlp(v)
+
+        out = rearrange(out, '(b h1 w1) head c (h w) -> b (head c) (h1 h) (w1 w)', head=self.num_heads, h1=Hx//self.window_size,
+                        w1=Wx//self.window_size, h=self.window_size, w=self.window_size)
+
+        return out[:, :, :H, :W]
+
+
+    def forward(self, x):
+        qkv = self.qkv_dwconv(self.qkv(x))
+        out = self.get_attn(qkv)
+        out = self.project_out(out)
+        return out
+    def flops(self, inp_shape):
+        C, H, W = inp_shape
+        flops = 0
+        # fc1
+        flops += H * W * C * C * 3
+        # dwconv
+        flops += H * W * (C * 3) * 3 * 3
+        # attn
+        c_attn = C // self.num_heads
+        flops += self.num_heads * 2 * ((c_attn ** 2) * H * W)
+        # MGate
+        flops += H * W * C * (self.window_size ** 2)
+        # fc2
+        flops += H * W * C * C
+        # print("Attn:{%.2f}" % (flops / 1e9))
+        return flops
+class FreqLCBlock(nn.Module):
+    def __init__(self, dim=32,
+                 num_heads=1,
+                 bias=False,
+                 LayerNorm_type='WithBias',
+                 window_size=8,
+                 cs='dct'):
+        # def __init__(self, dim, num_heads, ffn_expansion_factor, bias, LayerNorm_type):
+        super(FreqLCBlock, self).__init__()
+
+        self.dim = dim
+        self.window_size = window_size
+        self.cs = cs
+
+        if 'nodct' in cs:
+            self.dct = nn.Identity()
+            self.idct = nn.Identity()
+        elif 'dct_torch' in cs:
+            self.dct = DCT2_torch()
+            self.idct = IDCT2_torch()
+        else:
+            self.dct = DCT2x()
+            self.idct = IDCT2x()
+
+        self.norm1 = LayerNorm(dim, LayerNorm_type)
+        self.attn = Attention(dim, num_heads, bias, window_size=window_size)
+
+    def forward(self, x):
+        x_dct = self.dct(x)
+        x_attn = self.attn(self.norm1(x_dct))
+        x_dct = x_dct + x_attn
+        x = self.idct(x_dct)
+
+        return x
+    def flops(self, inp_shape):
+        C, H, W = inp_shape
+        flops = 0
+        # dct
+        if 'nodct' in self.cs:
+            flops += 0
+        else:
+            flops += self.dct.flops(inp_shape)
+            flops += self.idct.flops(inp_shape)
+        # LN * 1
+        flops += 1 * H * W * C
+        for blk in self.attn:
+            flops += blk.flops(inp_shape)
+        return flops
+
+if __name__=='__main__':
+    x = torch.randn(1, 32, 64, 64).cuda()
+    model1 = FreqLCBlock(32, cs='dct_torch').cuda()
+    model2 = FreqLCBlock(32, cs='dct').cuda()
+    model3 = FreqLCBlock(32, cs='nodct').cuda()
+    y1 = model1(x)
+    y2 = model2(x)
+    y3 = model3(x)
+    print(y1.shape, y2.shape, y3.shape)
---
+++ dct_util.py
@@ -0,0 +1,346 @@
+import torch
+from torch import nn as nn
+import numpy as np
+
+
+def dct(x, norm=None):
+    """
+    Discrete Cosine Transform, Type II (a.k.a. the DCT)
+
+    For the meaning of the parameter `norm`, see:
+    https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
+
+    :param x: the input signal
+    :param norm: the normalization, None or 'ortho'
+    :return: the DCT-II of the signal over the last dimension
+    """
+    x_shape = x.shape
+    N = x_shape[-1]
+    x = x.contiguous().view(-1, N)
+
+    v = torch.cat([x[:, ::2], x[:, 1::2].flip([1])], dim=1)
+    # print(v.shape)
+    Vc = torch.fft.fft(v, dim=1) # , onesided=False)
+    # print(Vc.shape)
+    k = - torch.arange(N, dtype=x.dtype, device=x.device)[None, :] * np.pi / (2 * N)
+    W_r = torch.cos(k)
+    W_i = torch.sin(k)
+
+    V = Vc.real * W_r - Vc.imag * W_i # [:, :N // 2 + 1]
+
+    if norm == 'ortho':
+        V[:, 0] /= np.sqrt(N) * 2
+        V[:, 1:] /= np.sqrt(N / 2) * 2
+
+    V = 2 * V.view(*x_shape)
+    # print(V)
+    return V
+
+def idct(X, norm=None):
+    """
+    The inverse to DCT-II, which is a scaled Discrete Cosine Transform, Type III
+
+    Our definition of idct is that idct(dct(x)) == x
+
+    For the meaning of the parameter `norm`, see:
+    https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
+
+    :param X: the input signal
+    :param norm: the normalization, None or 'ortho'
+    :return: the inverse DCT-II of the signal over the last dimension
+    """
+
+    x_shape = X.shape
+    N = x_shape[-1]
+
+    X_v = X.contiguous().view(-1, x_shape[-1]) / 2
+
+    if norm == 'ortho':
+        X_v[:, 0] *= np.sqrt(N) * 2
+        X_v[:, 1:] *= np.sqrt(N / 2) * 2
+    # print(X)
+    k = torch.arange(x_shape[-1], dtype=X.dtype, device=X.device)[None, :] * np.pi / (2 * N)
+    W_r = torch.cos(k)
+    W_i = torch.sin(k)
+
+    V_t_r = X_v
+    V_t_i = torch.cat([X_v[:, :1] * 0, -X_v.flip([1])[:, :-1]], dim=1)
+
+    V_r = V_t_r * W_r - V_t_i * W_i
+    V_i = V_t_r * W_i + V_t_i * W_r
+
+    # V = torch.cat([V_r.unsqueeze(2), V_i.unsqueeze(2)], dim=2)
+    V = torch.complex(V_r, V_i)
+    v = torch.fft.ifft(V, dim=1) # , onesided=False)
+    x = v.new_zeros(v.shape)
+    x[:, ::2] += v[:, :N - (N // 2)]
+    x[:, 1::2] += v.flip([1])[:, :N // 2]
+    x = x.real
+    return x.view(*x_shape)
+
+
+def dct_2d_torch(x, norm=None):
+    """
+    2-dimentional Discrete Cosine Transform, Type II (a.k.a. the DCT)
+
+    For the meaning of the parameter `norm`, see:
+    https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
+
+    :param x: the input signal
+    :param norm: the normalization, None or 'ortho'
+    :return: the DCT-II of the signal over the last 2 dimensions
+    """
+    X1 = dct(x, norm=norm)
+    X2 = dct(X1.transpose(-1, -2), norm=norm)
+    return X2.transpose(-1, -2)
+
+
+def idct_2d_torch(X, norm=None):
+    """
+    The inverse to 2D DCT-II, which is a scaled Discrete Cosine Transform, Type III
+
+    Our definition of idct is that idct_2d(dct_2d(x)) == x
+
+    For the meaning of the parameter `norm`, see:
+    https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.fftpack.dct.html
+
+    :param X: the input signal
+    :param norm: the normalization, None or 'ortho'
+    :return: the DCT-II of the signal over the last 2 dimensions
+    """
+    x1 = idct(X, norm=norm)
+    x2 = idct(x1.transpose(-1, -2), norm=norm)
+    return x2.transpose(-1, -2)
+
+def get_dctMatrix(m, n):
+    N = n
+    C_temp = np.zeros([m, n])
+    C_temp[0, :] = 1 * np.sqrt(1 / N)
+
+    for i in range(1, m):
+        for j in range(n):
+            C_temp[i, j] = np.cos(np.pi * i * (2 * j + 1) / (2 * N)) * np.sqrt(2 / N)
+    return torch.tensor(C_temp, dtype=torch.float)
+
+
+def dct1d(feature, dctMat):
+    feature = feature @ dctMat.T # dctMat @ feature  #
+    return feature.contiguous()  # torch.tensor(x, device=feature.device)
+
+
+def idct1d(feature, dctMat):
+    feature = feature @ dctMat # .T # dctMat.T @ feature  # .T
+    return feature.contiguous()  # torch.tensor(x, device=feature.device)
+
+
+def dct2d(feature, dctMat):
+    # print(dctMat.shape, feature.shape)
+    feature = dct1d(feature, dctMat)# dctMat @ feature
+    # print(dctMat.shape, feature.shape)
+    # feature = feature @ dctMat.T
+    # print(feature.transpose(-1, -2).shape, dctMat.shape)
+    feature = dct1d(feature.transpose(-1, -2), dctMat) # dctMat @ feature.transpose(-1, -2) # @ dctMat.T
+    return feature.transpose(-1, -2).contiguous()  # torch.tensor(x, device=feature.device)
+
+
+def idct2d(feature, dctMat):
+    feature = idct1d(feature, dctMat) # dctMat.T @ feature # .transpose(-1, -2)
+    feature = idct1d(feature.transpose(-1, -2), dctMat)
+    return feature.transpose(-1, -2).contiguous() # torch.tensor(x, device=feature.device)
+
+def dct2dx(feature, dctMat1, dctMat2):
+    # print(dctMat.shape, feature.shape)
+    feature = dct1d(feature, dctMat1) # dctMat1 @ feature
+    # print(dctMat.shape, feature.shape)
+    feature = dct1d(feature.transpose(-1, -2), dctMat2) # feature @ dctMat2.T
+    return feature.transpose(-1, -2).contiguous()  # torch.tensor(x, device=feature.device)
+
+
+def idct2dx(feature, dctMat1, dctMat2):
+    feature = idct1d(feature, dctMat1)  # dctMat.T @ feature # .transpose(-1, -2)
+    feature = idct1d(feature.transpose(-1, -2), dctMat2)
+    return feature.transpose(-1, -2).contiguous()  # torch.tensor(x, device=feature.device)
+
+class DCT1d(nn.Module):
+    def __init__(self, window_size=64):
+        super(DCT1d, self).__init__()
+        self.dctMat = get_dctMatrix(window_size, window_size)
+
+    def forward(self, x):
+        self.dctMat = self.dctMat.to(x.device)
+        # print(x.shape, self.dctMat.shape)
+        x = dct1d(x, self.dctMat)
+        return x
+
+
+class IDCT1d(nn.Module):
+    def __init__(self, window_size=64):
+        super(IDCT1d, self).__init__()
+        self.dctMat = get_dctMatrix(window_size, window_size)
+
+    def forward(self, x):
+        self.dctMat = self.dctMat.to(x.device)
+        x = idct1d(x, self.dctMat)
+        return x
+class DCT1x(nn.Module):
+    def __init__(self, dim=-1):
+        super(DCT1x, self).__init__()
+        self.dctMat = None
+        self.dim = dim
+
+    def check_dct_matrix(self, d):
+        if self.dctMat is None or d != self.dctMat.shape[-1]:
+            self.dctMat = get_dctMatrix(d, d)
+
+    def forward(self, x):
+        if self.dim != -1 or self.dim != len(x.shape)-1:
+            x = x.transpose(self.dim, -1)
+        self.check_dct_matrix(x.shape[-1])
+
+        self.dctMat = self.dctMat.to(x.device)
+        # print(x.shape, self.dctMat.shape)
+        x = dct1d(x, self.dctMat)
+        if self.dim != -1 or self.dim != len(x.shape)-1:
+            x = x.transpose(self.dim, -1)
+        return x.contiguous()
+
+
+class IDCT1x(nn.Module):
+    def __init__(self, dim=-1):
+        super(IDCT1x, self).__init__()
+        self.dctMat = None
+        self.dim = dim
+    def check_dct_matrix(self, d):
+        if self.dctMat is None or d != self.dctMat.shape[-1]:
+            self.dctMat = get_dctMatrix(d, d)
+    def forward(self, x):
+        if self.dim != -1 or self.dim != len(x.shape) - 1:
+            x = x.transpose(self.dim, -1)
+        self.check_dct_matrix(x.shape[-1])
+
+        self.dctMat = self.dctMat.to(x.device)
+        # print(x.shape, self.dctMat.shape)
+        x = idct1d(x, self.dctMat)
+        if self.dim != -1 or self.dim != len(x.shape) - 1:
+            x = x.transpose(self.dim, -1)
+        return x.contiguous()
+
+class DCT2(nn.Module):
+    def __init__(self, window_size=8, norm='ortho'):
+        super(DCT2, self).__init__()
+        self.dctMat = get_dctMatrix(window_size, window_size)
+        self.norm = norm
+        self.window_size = window_size
+    def forward(self, x):
+        dctMat = self.dctMat.to(x.device)
+        # print(x.shape, self.dctMat.shape)
+        x = dct2d(x, dctMat)
+        return x
+class DCT2_torch(nn.Module):
+    def __init__(self, norm='ortho'):
+        super(DCT2_torch, self).__init__()
+        self.norm = norm
+    def forward(self, x):
+        # print(x.shape)
+        x = dct_2d_torch(x, self.norm)
+        return x
+    def flops(self, inp_shape):
+        C, H, W = inp_shape
+        flops = 0
+        flops += C * H * W * np.log2(H * W)
+        return flops
+class IDCT2_torch(nn.Module):
+    def __init__(self, norm='ortho'):
+        super(IDCT2_torch, self).__init__()
+        self.norm = norm
+    def forward(self, x):
+        # print(x.shape)
+        x = idct_2d_torch(x, self.norm)
+        return x
+    def flops(self, inp_shape):
+        C, H, W = inp_shape
+        flops = 0
+        flops += C * H * W * np.log2(H * W)
+        return flops
+
+class IDCT2(nn.Module):
+    def __init__(self, window_size=8, norm='ortho'):
+        super(IDCT2, self).__init__()
+        self.dctMat = get_dctMatrix(window_size, window_size)
+        self.norm = norm
+        self.window_size = window_size
+    def forward(self, x):
+        dctMat = self.dctMat.to(x.device)
+        x = idct2d(x, dctMat)
+        return x
+
+class DCT2x(nn.Module):
+    def __init__(self, norm='ortho'):
+        super(DCT2x, self).__init__()
+        self.dctMatH = None
+        self.dctMatW = None
+        self.norm = norm
+
+    def check_dct_matrix(self, h, w):
+        if self.dctMatH is None or self.dctMatW is None:
+            self.dctMatH = get_dctMatrix(h, h)
+            self.dctMatW = get_dctMatrix(w, w)
+        elif h != self.dctMatH.shape[-1] and w != self.dctMatW.shape[-1]:
+            self.dctMatH = get_dctMatrix(h, h)
+            self.dctMatW = get_dctMatrix(w, w)
+        elif h != self.dctMatH.shape[-1]:
+            self.dctMatH = get_dctMatrix(h, h)
+            # self.dctMatH = self.dctMatH.to(x.device)
+        elif w != self.dctMatW.shape[-1]:
+            self.dctMatW = get_dctMatrix(w, w)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        self.check_dct_matrix(h, w)
+        dctMatH = self.dctMatH.to(x.device)
+        dctMatW = self.dctMatW.to(x.device)
+        # print(x.shape, self.dctMatH.shape, self.dctMatW.shape)
+        x = dct2dx(x, dctMatW, dctMatH)
+
+        return x
+
+
+class IDCT2x(nn.Module):
+    def __init__(self, norm='ortho'):
+        super(IDCT2x, self).__init__()
+        self.dctMatH = None
+        self.dctMatW = None
+        self.norm = norm
+
+    def check_dct_matrix(self, h, w):
+        if self.dctMatH is None or self.dctMatW is None:
+            self.dctMatH = get_dctMatrix(h, h)
+            self.dctMatW = get_dctMatrix(w, w)
+        elif h != self.dctMatH.shape[-1] and w != self.dctMatW.shape[-1]:
+            self.dctMatH = get_dctMatrix(h, h)
+            self.dctMatW = get_dctMatrix(w, w)
+        elif h != self.dctMatH.shape[-1]:
+            self.dctMatH = get_dctMatrix(h, h)
+            # self.dctMatH = self.dctMatH.to(x.device)
+        elif w != self.dctMatW.shape[-1]:
+            self.dctMatW = get_dctMatrix(w, w)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        self.check_dct_matrix(h, w)
+        dctMatH = self.dctMatH.to(x.device)
+        dctMatW = self.dctMatW.to(x.device)
+        x = idct2dx(x, dctMatW, dctMatH)
+
+        return x
+
+
+if __name__=='__main__':
+    x = torch.randn(1, 8, 64, 64).cuda()
+    model1 = DCT2x().cuda()
+    model2 = DCT2_torch().cuda()
+
+    y = model1(x)
+    y_ = model2(x)
+
+    print(torch.sum(y-y_))
